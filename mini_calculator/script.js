11c25fb26545d7c9467bf50da88fa91055e2b1a0
Snehalatha <133950677+SnehalathaC@users.noreply.github.com>
2023-07-26 20:00:00 +0530

    Add files via upload

---
+++ mini_calculator/script.js
@@ -0,0 +1,32 @@
+function add() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 + num2;
+    alert('Result: ' + result);
+  }
+  
+  function subtract() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 - num2;
+    alert('Result: ' + result);
+  }
+  
+  function divide() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    if (num2 === 0) {
+      alert("Cannot divide by zero!");
+      return;
+    }
+    const result = num1 / num2;
+    alert('Result: ' + result);
+  }
+  
+  function multiply() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const result = num1 * num2;
+    alert('Result: ' + result);
+  }
+  
